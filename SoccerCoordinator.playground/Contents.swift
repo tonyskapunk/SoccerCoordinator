9e7eb4a4533c81fbc745687155f216714a228677
Luis Corvalan <LRCSiCloud@icloud.com>
2016-06-19 12:44:05 -0500

    First Version

---
+++ SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,139 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Dictionary for each player
+let player1: [String: Any] = [
+    "Name": "Joe Smith",
+    "Height (Inches)": 42,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Jim and Jan Smith"
+]
+
+let player2: [String: Any] = [
+    "Name": "Jill Tanner",
+    "Height (Inches)": 36,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Clara Tanner"
+]
+
+let player3: [String: Any] = [
+    "Name": "Bill Bon",
+    "Height (Inches)": 43,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Sara and Jenny Bon"
+]
+
+let player4: [String: Any] = [
+    "Name": "Eva Gordon",
+    "Height (Inches)": 45,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "Wendy amd Mike Gordon"
+]
+
+let player5: [String: Any] = [
+    "Name": "Matt Gill",
+    "Height (Inches)": 40,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "Charles and Sylvia Gill"
+]
+
+let player6: [String: Any] = [
+    "Name": "Kimmy Stein",
+    "Height (Inches)": 41,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "Bill and Hillary Stein"
+]
+
+let player7: [String: Any] = [
+    "Name": "Sammy Adams",
+    "Height (Inches)": 45,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "Jeff Adams"
+]
+
+let player8: [String: Any] = [
+    "Name": "Karl Saygan",
+    "Height (Inches)": 42,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Heather Bledsoe"
+]
+
+let player9: [String: Any] = [
+    "Name": "Suzane Greenberg",
+    "Height (Inches)": 44,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Henrietta Dumas"
+]
+
+let player10: [String: Any] = [
+    "Name": "Sal Dali",
+    "Height (Inches)": 41,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "Gala Dali"
+]
+
+let player11: [String: Any] = [
+    "Name": "Joe Kavalier",
+    "Height (Inches)": 39,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "Sam and Elaine Kavalier"
+]
+
+let player12: [String: Any] = [
+    "Name": "Ben Finkelstein",
+    "Height (Inches)": 44,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "Aaron and Jill Finkelstein"
+]
+
+let player13: [String: Any] = [
+    "Name": "Diego Soto",
+    "Height (Inches)": 41,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Robin and Sarika Soto"
+]
+
+let player14: [String: Any] = [
+    "Name": "Chole Alaska",
+    "Height (Inches)": 47,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "David and Jamie Alaska"
+]
+
+let player15: [String: Any] = [
+    "Name": "Arnold Wills",
+    "Height (Inches)": 43,
+    "Soccer Experience": false,
+    "Guardian Name(s)": "Claire Wills"
+]
+
+let player16: [String: Any] = [
+    "Name": "Phillp Helm",
+    "Height (Inches)": 44,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Thomas Helm and Eva Jones"
+]
+
+let player17: [String: Any] = [
+    "Name": "Les Clay",
+    "Height (Inches)": 42,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Wynonna Brown"
+]
+
+let player18: [String: Any] = [
+    "Name": "Herschel Krustofskit",
+    "Height (Inches)": 45,
+    "Soccer Experience": true,
+    "Guardian Name(s)": "Hyman and Rachel Krustofski"
+]
+
+
+// Array of Dictionaries for Entire League
+var entireLeague:[[String: Any]] = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+
+
+// Array of Dictionaries for each of three teams
+var team1: [[String: Any]] = []
+
